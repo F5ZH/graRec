d1fbb4b3c81d918dc8dcc71a2a17f7ee51f75fdd
fzh <1976958279@qq.com>
2025-09-16 19:15:54 +0800

    add:swin BBN

--- order.sh
+++ order.sh
@@ -1,4 +1,4 @@
-# 400训练，课程学习推迟到20轮往后 抗长尾分布未启动
+# 400ConvNeXt训练，课程学习推迟到20轮往后 抗长尾分布未启动
 # python train_dual_branch.py --mode train \
 #   --train_dir data/WebFG-400/train \
 #   --outdir runs/dual_branch_v1 \
@@ -17,6 +17,26 @@
 #   --local_crop_size 224 \
 #   --lr 5e-4 \
 #   --channels_last --grad_ckpt --microbatch 16
+
+# 400Swin训练，课程学习推迟到20轮往后 抗长尾分布未启动
+# python train_dual_branch.py --mode train \
+#   --train_dir data/WebFG-400/train \
+#   --outdir runs/dual_branch_v1 \
+#   --arch swin_base \
+#   --epochs 20 \
+#   --img_size 384 \
+#   --batch_size 64 \
+#   --loss elrplus --elr_lambda 2.0 \
+#   --mixup 0 --cutmix 0 --randaug_N 2 --randaug_M 10 \
+#   --use_wsdan \
+#   --arcface --margin 0.15 --scale 20 \
+#   --pretrained --freeze_backbone_epochs 1 --backbone_lr_mult 0.1 \
+#   --workers 8 --amp \
+#   --lambda_global 1.0 \
+#   --lambda_local 0.5 \
+#   --local_crop_size 224 \
+#   --lr 5e-4 \
+#   --grad_ckpt --microbatch 16
 
 # 预测
 python train_dual_branch.py --mode predict \
