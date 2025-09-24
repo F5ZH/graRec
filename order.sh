6ce392c251fc746543dab730cbbb8ec4e66a55d5
fzh <1976958279@qq.com>
2025-09-24 21:42:48 +0800

    order update

--- order.sh
+++ order.sh
@@ -1,8 +1,8 @@
 #训练
 python train.py --pretrained \
-  --output_dir ./runs/WebFG-400-reweight \
+  --output_dir ./runs/WebFG-400-base \
 && \
-tensorboard --logdir=runs/WebFG-400-reweight/logs \
+tensorboard --logdir=runs/WebFG-400-base/logs \
 && \
 #预测
 python predict.py
